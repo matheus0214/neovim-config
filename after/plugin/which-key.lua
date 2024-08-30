e176b4e4ae081a3a94143d5ff0d2f51848eb4dfb
Matheus G. Dias <matheusgiga123@gmail.com>
2024-08-30 13:12:22 -0300

    configure terminal

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -1,5 +1,20 @@
 local map = vim.keymap.set
 local wk = require("which-key")
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 local Terminal = require('toggleterm.terminal').Terminal
 local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
 
@@ -58,13 +73,11 @@ local mappings = {
 
   { "<space>t",   group = "Terminal" },
   { "<space>tf",  "<cmd>ToggleTerm<cr>",                         desc = "Open terminal float" },
+  { "<space>tv",  "<cmd>ToggleTerm direction=horizontal<cr>",    desc = "Open terminal bottom" },
 
-  -- { "<S-h>",      "<cmd>tabprevious<cr>",                        desc = "Previous tab" },
-  -- { "<S-l>",      "<cmd>tabnext<cr>",                            desc = "Previous next" }
   { "<S-l>",      ":BufferNext<cr>",                            desc = "Next tab" },
   { "<S-h>",      ":BufferPrevious<cr>",                            desc = "Previous tab" },
   { "<leader>c",      ":BufferClose<cr>",                            desc = "Close tab" },
-
 }
 
 map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
@@ -72,6 +85,14 @@ map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
+-- Resize window
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Clear search with <esc>
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
 
 wk.add(mappings, {})
 wk.add({
