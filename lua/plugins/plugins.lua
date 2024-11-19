b7f9fb0fbb544d026faa81b6238df7f10ba5fa91
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-19 18:18:14 -0300

    format

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,48 +1,48 @@
 return {
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = {
-      'kevinhwang91/promise-async'
-    }
-  },
-  { "lewis6991/gitsigns.nvim" },
-  { "gelguy/wilder.nvim" },
-  { "windwp/nvim-ts-autotag" },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-nvim-lsp',
-    {
-      'L3MON4D3/LuaSnip',
-      dependencies = {
-        'saadparwaiz1/cmp_luasnip',
-        'rafamadriz/friendly-snippets'
-      }
-    }
-  },
-  { "tpope/vim-commentary" },
-  {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { 'nvim-telescope/telescope-ui-select.nvim' },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-  },
-  { 'williamboman/mason.nvim' },
-  { 'williamboman/mason-lspconfig.nvim' },
-  { 'neovim/nvim-lspconfig' },
-  { "rose-pine/neovim",                 name = "rose-pine" },
-  { "folke/zen-mode.nvim" },
-  { "stevearc/conform.nvim"},
-  { "WhoIsSethDaniel/mason-tool-installer.nvim"},
-  { "mfussenegger/nvim-lint"}
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+		},
+	},
+	{ "lewis6991/gitsigns.nvim" },
+	{ "gelguy/wilder.nvim" },
+	{ "windwp/nvim-ts-autotag" },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = {
+				"saadparwaiz1/cmp_luasnip",
+				"rafamadriz/friendly-snippets",
+			},
+		},
+	},
+	{ "tpope/vim-commentary" },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{ "nvim-telescope/telescope-ui-select.nvim" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "folke/zen-mode.nvim" },
+	{ "stevearc/conform.nvim" },
+	{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
+	{ "mfussenegger/nvim-lint" },
 }
