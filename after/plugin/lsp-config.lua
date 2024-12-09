ad51e4dc85e4d0557d2dd495d4a5e22ee716b968
Matheus G. Dias <matheusgiga123@gmail.com>
2024-12-09 18:13:55 -0300

    refactor: remove cmp

--- after/plugin/lsp-config.lua
+++ after/plugin/lsp-config.lua
@@ -3,8 +3,6 @@ local mason_lsp = require("mason-lspconfig")
 local nvim_lspconfig = require("lspconfig")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 mason.setup()
 mason_lsp.setup({
 	ensure_installed = { "biome", "lua_ls", "ts_ls", "clangd", "rust_analyzer", "pyright" },
