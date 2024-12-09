e9e44923986a2cfc2976b493d28df4b7fc42050b
Matheus G. Dias <matheusgiga123@gmail.com>
2024-12-09 18:29:32 -0300

    refactor: options

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,4 @@
 vim.opt.backup = false -- creates a backup file
-vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
 vim.opt.colorcolumn = "" -- fixes indentline for now
 vim.opt.completeopt = { "menuone", "noselect" }
@@ -11,9 +10,7 @@ vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applicati
 vim.opt.hidden = true -- required to keep multiple buffers and open multiple buffers
 vim.opt.hlsearch = true -- highlight all matches on previous search pattern
 vim.opt.ignorecase = true -- ignore case in search patterns
-vim.opt.mouse = "" -- allow the mouse to be used in neovim
 vim.opt.pumheight = 10 -- pop up menu height
-vim.opt.showmode = true-- we don't need to see things like -- INSERT -- anymore
 vim.opt.showtabline = 1 -- always show tabs, 2 show tabs
 vim.opt.smartcase = true -- smart case
 vim.opt.smartindent = true -- make indenting smarter again
@@ -28,27 +25,20 @@ vim.opt.updatetime = 300 -- faster completion
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
 vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 
 vim.opt.signcolumn = "yes" -- always show the sign column otherwise it would shift the text each time
 vim.opt.wrap = true -- display lines as one long line
 vim.opt.spell = false
 vim.opt.spelllang = "en"
-vim.opt.scrolloff = 8 -- is one of my fav
+vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.textwidth = 80
 
-vim.opt.cursorline = true -- highlight the current line
-vim.opt.number = true -- set numbered lines
-vim.opt.relativenumber = true-- set relative numbered lines
-vim.opt.numberwidth = 1-- set number column width to 2 {default 4}
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 1
 
-vim.o.foldcolumn = '0' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-
-vim.opt.timeoutlen = 300 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.timeoutlen = 300
 vim.opt.guicursor = "n-v-i-c:block"
-
-
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 2
