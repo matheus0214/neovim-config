60a6bfbe08f9f5bcb2ee085722a17b0a25fe4c13
Matheus G. Dias <matheusgiga123@gmail.com>
2024-12-10 22:58:43 -0300

    feat: add cmp

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -30,4 +30,16 @@ return {
 		},
 	},
 	{ "cpwrs/americano.nvim" },
+	{ "onsails/lspkind.nvim" },
+	{
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = {
+				"saadparwaiz1/cmp_luasnip",
+				"rafamadriz/friendly-snippets",
+			},
+		},
+	},
 }
